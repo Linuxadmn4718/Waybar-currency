26f985a42e4096d7f4d4a5ed541a06dfe0d2b77b
Linuxadmn4718 <137133494+Linuxadmn4718@users.noreply.github.com>
2025-02-08 19:56:23 +0800

    Delete show_graph.sh

--- show_graph.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generate the plot using gnuplot
-/usr/bin/gnuplot $HOME/.config/hypr/waybar/scripts/Currency/plot_rates.gp
-
-# Command to display the image
-/usr/bin/yad --size=FIT --picture $HOME/.config/hypr/waybar/scripts/Currency/rates.png 
